510bd36dc7753199e039a2023c689164941976c3
kausti18 <kaustubh_ug@ece.nits.ac.in>
2022-06-16 19:38:18 +0530

    node and express is added

---
+++ app.js
@@ -0,0 +1,19 @@
+//imports
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+//Static  Files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
+
+app.get('',(req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+})
+
+
+//listen on port 3000
+app.listen(port)
