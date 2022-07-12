f260e5b9b3018a64f970f3e312915aa7d0e3303e
robinanand000 <robinsanand222@gmail.com>
2022-07-12 21:10:31 +0530

    updated about page

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(require('express-session')({
     saveUninitialize: false
 }));
 
-// mongoose.connect("mongodb+srv://admin-cdac:Admin%40cdacsilchar@cdac.isrtcby.mongodb.net/cdac", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-cdac:Admin%40cdacsilchar@cdac.isrtcby.mongodb.net/cdac", {useNewUrlParser: true});
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded('extended: true'));
